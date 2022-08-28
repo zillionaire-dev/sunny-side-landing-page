75f0a15be859b3e5906130f8131285b97ea8562d
zillionaire-dev <101994184+zillionaire-dev@users.noreply.github.com>
2022-08-28 19:22:44 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const toggleBtn = document.querySelector(".toggle-btn");
 const navBar = document.querySelector(".nav-links");
-const pop = document.querySelector(".pop")
 
 toggleBtn.addEventListener("click", () => {
-  if (navBar.classList.contains("show") && pop.classList.contains("show")) {
+  if (navBar.classList.contains("show")) {
     navBar.classList.remove("show");
-    pop.classList.remove("show")
-    toggleBtn.src = "./images/icon-hamburger.svg"
+    toggleBtn.src = "/images/icon-hamburger.svg";
   } else {
-    navBar.classList.add("show")
-    pop.classList.add("show")
-    toggleBtn.src = "./images/icon-close.svg"
+    navBar.classList.add("show");
+    toggleBtn.src = "/images/icon-close.svg";
   }
 })
